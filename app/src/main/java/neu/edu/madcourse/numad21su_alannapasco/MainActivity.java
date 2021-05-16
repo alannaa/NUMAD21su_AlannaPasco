06c8c3f8a1cad2fe57c13f505bb9fb9a01116ce0
Alanna <pasco.rae@gmail.com>
2021-05-16 14:26:45 -0400

    Test init commit

---
+++ app/src/main/java/neu/edu/madcourse/numad21su_alannapasco/MainActivity.java
@@ -0,0 +1,16 @@
+package neu.edu.madcourse.numad21su_alannapasco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //Test to check if git is working
+}
