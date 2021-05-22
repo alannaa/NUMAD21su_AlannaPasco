c1c1f42601a359635b037944bf1baea058b13391
Alanna <pasco.rae@gmail.com>
2021-05-22 17:03:02 -0400

    Added comments to code to explain each method

--- app/src/main/java/neu/edu/madcourse/numad21su_alannapasco/MainActivity.java
+++ app/src/main/java/neu/edu/madcourse/numad21su_alannapasco/MainActivity.java
@@ -13,6 +13,8 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    String language = "en";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,35 +22,47 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void displayAbout(View view) {
+        //Set the context to the appropriate language
+        Context context = LocaleHelper.setLocale(MainActivity.this, language);
+        Resources resources = context.getResources();
+        //Grab the aboutText TextView
         TextView aboutText = findViewById(R.id.aboutText);
-
+        //If the space is currently empty, populate the aboutText (in the cur language)
+        //If the space currently displays the aboutText, remove it.
         if (aboutText.getText().toString().isEmpty()) {
-            aboutText.setText(R.string.aboutText_string);
+            aboutText.setText(resources.getString(R.string.aboutText_string));
         } else {
-            aboutText.setText(R.string.no_value_string);
+            aboutText.setText(resources.getString(R.string.no_value_string));
         }
     }
 
-    String language = "en";
-    // Attempt to "toggle" between arabic and english text for fun.
-    // I could not figure how to change all text to arabic in one line, so I manually
-    // reset all strings over again to make them display in arabic. I'm sure there is a cleaner
-    // way to do this and would love help/feedback.
+    // Attempt to translate app between English, Arabic, and Spanish, for fun
+    // I could not figure how to change all text in one line, so I manually
+    // reset all strings over again to make them display in the new language.
+    // I'm sure there is a cleaner way to do this and would love help/feedback.
     public void toggleLanguage(View view) {
-        if (language.equals("en")){
-            language = "ar";
-        } else {
-            language = "en";
+        switch(language) {
+            case "en":
+                language = "ar";
+                break;
+            case "ar":
+                language = "es";
+                break;
+            default:
+                language = "en";
         }
         Context context = LocaleHelper.setLocale(MainActivity.this, language);
-
         Resources resources = context.getResources();
 
         TextView helloWorldText = findViewById(R.id.helloWorldView);
         helloWorldText.setText(resources.getString(R.string.helloWorld_string));
 
         TextView aboutText = findViewById(R.id.aboutText);
-        aboutText.setText(resources.getString(R.string.aboutText_string));
+        if (aboutText.getText().toString().isEmpty()) {
+            aboutText.setText(resources.getString(R.string.no_value_string));
+        } else {
+            aboutText.setText(resources.getString(R.string.aboutText_string));
+        }
 
         TextView aboutButton = findViewById(R.id.aboutButton);
         aboutButton.setText(resources.getString(R.string.aboutButton_string));
