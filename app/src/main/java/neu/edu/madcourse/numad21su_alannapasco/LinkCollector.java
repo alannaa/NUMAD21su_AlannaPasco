6123f85c093c6c5033156c332a8822004a68b873
Alanna <pasco.rae@gmail.com>
2021-06-05 23:00:32 -0400

    adjusted buttons on main page, fixed bug where I instantiated the FAB as a regular Button object

--- app/src/main/java/neu/edu/madcourse/numad21su_alannapasco/LinkCollector.java
+++ app/src/main/java/neu/edu/madcourse/numad21su_alannapasco/LinkCollector.java
@@ -2,7 +2,11 @@ package neu.edu.madcourse.numad21su_alannapasco;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class LinkCollector extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class LinkCollector extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_link_collector);
+
+        FloatingActionButton linkCFAB = findViewById(R.id.link_collector_FAB_id);
+        linkCFAB.setOnClickListener(v -> fabListener());
+    }
+
+
+    // Allows User to add a link to the link collector via a custom dialogue
+    // after the new link is added, a snackbar is displayed with the outcome of the add
+    public void fabListener() {
+
+
     }
 }
