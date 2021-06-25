0232451aaffda869b89b6cc0f5b69deef70e7fd9
Alanna <pasco.rae@gmail.com>
2021-06-25 14:30:56 -0400

    Set up ays button on main activity, started designing layout of ays activity

---
+++ app/src/main/java/neu/edu/madcourse/numad21su_alannapasco/AtYourService/AtYourService.java
@@ -0,0 +1,16 @@
+package neu.edu.madcourse.numad21su_alannapasco.AtYourService;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import neu.edu.madcourse.numad21su_alannapasco.R;
+
+public class AtYourService extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_at_your_service);
+    }
+}
